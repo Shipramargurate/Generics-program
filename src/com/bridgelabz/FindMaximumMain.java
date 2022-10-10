b397acf687e9c7c3dfd869ef097715fa20b39520
shipramargurate <shivanikumari22322@gmail.com>
2022-10-10 21:34:08 +0530

    [shivani] : Added functionality to find maximum by using Generics

--- src/com/bridgelabz/FindMaximumMain.java
+++ src/com/bridgelabz/FindMaximumMain.java
@@ -3,7 +3,13 @@ package com.bridgelabz;
 public class FindMaximumMain {
 	public static void main(String[] args) {
 		FindMaximum maximum = new FindMaximum();
+		System.out.println("Maximumn in given Integerr is :");
+		maximum.maximum(99, 99999, 999);
+		System.out.println();
 		System.out.println("Maximumn in given floats is :");
-		maximum.checkMaximum("Apple", "Peach", "Banana");
+		maximum.maximum(1.1, 2.2, 3.3);
+		System.out.println();
+		System.out.println("Maximumn in given Strings is :");
+		maximum.maximum("Apple", "Peach", "Banana");
 	}
 }
